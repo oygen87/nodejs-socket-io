3fb78eb1c49051fb05c32676ee79cb4afb32011a
Eugen Cazacu <eugen.cazacu@cygni.se>
2019-09-11 15:53:29 +0200

    change messages endpoint to post with a json body

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 var app = require("express")();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
+var bodyParser = require("body-parser");
 var cors = require("cors");
 
 const SERVER_PORT = 4000;
 
+app.use(bodyParser.json());
 app.use(cors());
 
 /**
- * in memory database
- * 
- * key repo: string
+ * in-memory database
+ *
+ * key: string
  * value: Array<{repo: string, message: string, username: string}>
  */
 const db = {};
 
-app.get("/messages/:repo", function(req, res) {
-  if (db[req.params.repo]) {
-    res.json({ data: db[req.params.repo] });
+app.post("/messages", function(req, res) {
+  if (db[req.body.repo]) {
+    res.json({ data: db[req.body.repo] });
   } else {
-    res.json({ data: [] });
+    db[req.body.repo] = [];
+    res.json({ data: db[req.body.repo] });
   }
 });
 
 io.on("connection", function(socket) {
   socket.on("clientMessageEvent", function(payload) {
-    if (payload.message.trim().length !== 0) {
+    if (
+      payload.message.trim().length !== 0 &&
+      payload.username.trim().length !== 0 &&
+      payload.repo.trim().length !== 0
+    ) {
       if (db[payload.repo]) {
         db[payload.repo].push(payload);
         io.emit(`serverMessageEvent:${payload.repo}`, db[payload.repo]);
@@ -37,7 +44,7 @@ io.on("connection", function(socket) {
     }
   });
   socket.on("disconnect", function() {
-    console.log("user disconnected");
+    // console.log("user disconnected");
   });
 });
 
