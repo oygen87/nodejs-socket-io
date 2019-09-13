fbfca647653acbfe0cd863cd566f042f834e288d
Eugen Cazacu <eugen.cazacu@cygni.se>
2019-09-13 09:32:57 +0200

    fetch github repo events

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 var bodyParser = require("body-parser");
 var cors = require("cors");
+var fetch = require("node-fetch");
 
 const SERVER_PORT = 4000;
 
@@ -14,8 +15,7 @@ app.use(cors());
  *
  * key: string
  * value: Array<{id?: string, repo: string, message: string, username: string}>
- * 
- * id is added server side
+ *
  */
 const db = {};
 
@@ -28,10 +28,42 @@ app.post("/messages", function(req, res) {
   }
 });
 
+app.post("/github-events", function(req, res) {
+  const user = req.body.repo.split("/")[3];
+  const repo = req.body.repo.split("/")[4];
+  const url = `https://api.github.com/repos/${user}/${repo}/events`;
+  fetch(url)
+    .then(res => res.json())
+    .then(json => res.json(json.map(mapEvents).filter()));
+});
+
+const mapEvents = e => {
+  return {
+    id: e.id,
+    actor: e.actor.display_login,
+    avatar: e.actor.avatar_url,
+    action: e.payload.action ? e.payload.action : null,
+    type: e.type.slice(0, -5),
+    issue: e.payload.issue ? e.payload.issue.url : null,
+    comment: e.payload.comment ? e.payload.comment.url : null,
+    pull_request: e.payload.pull_request ? e.payload.pull_request : null,
+    created: e.created_at
+  };
+};
+
+const filterEvents = e => {
+  return (
+    e.type !== "Watch" &&
+    e.type !== "Fork" &&
+    e.type !== "Follow" &&
+    e.type !== "Download"
+  );
+};
+
 io.on("connection", function(socket) {
   socket.on("clientMessageEvent", function(payload) {
     if (validatePayload(payload)) {
-      payload.id = (Math.random() * 10) + ""; // generate key
+      payload.id = Math.random() * 10 + "";
       if (db[payload.repo]) {
         db[payload.repo].push(payload);
         io.emit(`serverMessageEvent:${payload.repo}`, db[payload.repo]);
