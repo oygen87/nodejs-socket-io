3679b21e113b8057006c66c8324231f4deba124a
Eugen Cazacu <eugen.cazacu@cygni.se>
2019-09-15 19:19:49 +0200

    transform events to more readable text

--- utils/index.js
+++ utils/index.js
@@ -3,12 +3,30 @@ exports.mapEvents = e => {
     const issueUrl = e.payload.issue ? e.payload.issue.html_url : null;
     const pullRequestUrl = e.payload.pull_request ? e.payload.pull_request.html_url : null;
     const url = issueUrl ? issueUrl : pullRequestUrl ? pullRequestUrl : null;
+    let action = e.payload.action ? e.payload.action : null;
+    let type = e.type.slice(0, -5);
+
+    if (type.includes("Comment")) {
+        action = null;
+        if (type.includes("PullRequest")) {
+            type = "commented pull request"
+        }
+        if (type.includes("Issue")) {
+            type = "commented issue"
+        }
+        if (type.includes("Commit")) {
+            type = "commented commit"
+        }
+    }
+    type = type === "Issues" ? "Issue" : type;
+    type = type === "Push" ? "Pushed to " + e.payload.ref : type;
+    
     return {
       id: e.id,
       actor: e.actor.display_login,
       avatar: e.actor.avatar_url,
-      action: e.payload.action ? e.payload.action : null,
-      type: e.type.slice(0, -5),
+      action,
+      type,
       url,
       created: e.created_at,
     };
